55db4192a8c0d8e71599aae891cf4c75d7c9ead1
mafowillson <fouotsawilson@gmail.com>
2026-03-20 13:53:55 +0100

    Refactor pattern exercises and implement sorting algorithms
    - Updated right-angled triangle pattern to include an additional row.
    - Removed diamond pattern notebook as it was redundant.
    - Added linear search, binary search, bubble sort, insertion sort, selection sort, and sum of list elements implementations.
    - Introduced functions to generate number triangles and pyramid patterns.
    - Enhanced largest element finder with multiple implementations.

---
+++ 10-Searching-and-sorting-algorithms/bubble_sort.py
@@ -0,0 +1,14 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for passes in range(n):
+        for j in range(0, n-1-passes):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
+
+unsorted_list =[5, 4, 3, 2, 1] #[120, 25, 11, 34, 90, 22]
+sorted_list = bubble_sort(unsorted_list)
+print("Sorted elemets: ", sorted_list)
